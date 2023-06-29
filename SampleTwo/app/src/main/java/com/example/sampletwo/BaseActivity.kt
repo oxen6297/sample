f98582fe660936b98c4818f23c30d14cd87354c4
park <oxen6297@gmail.com>
2023-06-29 09:09:24 +0900

    refactor: 미사용 import 삭제

--- SampleTwo/app/src/main/java/com/example/sampletwo/BaseActivity.kt
+++ SampleTwo/app/src/main/java/com/example/sampletwo/BaseActivity.kt
@@ -4,7 +4,6 @@ import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.viewbinding.ViewBinding
 
