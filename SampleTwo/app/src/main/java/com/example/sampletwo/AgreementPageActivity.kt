ce3e37f0fd79717d4596b4c41a89e97f651d97e6
park <oxen6297@gmail.com>
2023-06-29 12:18:27 +0900

    refactor: 동의화면 페이지 클릭 이벤트 및 뷰모델 선언 수정

--- SampleTwo/app/src/main/java/com/example/sampletwo/AgreementPageActivity.kt
+++ SampleTwo/app/src/main/java/com/example/sampletwo/AgreementPageActivity.kt
@@ -1,74 +1,48 @@
 package com.example.sampletwo
 
-import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
-import androidx.lifecycle.ViewModelProvider
+import android.widget.Button
+import androidx.activity.viewModels
 import com.example.sampletwo.databinding.ActivityAgreementPageBinding
 
 class AgreementPageActivity : BaseActivity<ActivityAgreementPageBinding>(
     ActivityAgreementPageBinding::inflate
 ) {
-    private lateinit var mainViewModel: MainViewModel
+    private val mainViewModel: MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        mainViewModel = ViewModelProvider(this)[MainViewModel::class.java]
-        binding.appBar.imgBack.setOnClickListener {
-            finish()
-        }
-        binding.btnConfirm.setOnClickListener {
-            Log.d("click", "true")
-        }
-        clickCheckBox()
+        setBinding()
         observeData()
         isAgree()
     }
 
-    private fun clickCheckBox() {
-        binding.run {
-            textAgreeOne.setOnClickListener {
+    private fun setBinding() {
+        binding.apply {
+            appBar.imgBack.setOnClickListener {
+                finish()
+            }
+            btnConfirm.setOnClickListener {
+                Log.d("click", "true")
+            }
+            layoutAgreeOne.setOnClickListener {
                 mainViewModel.agreeOne.value = !(mainViewModel.agreeOne.value ?: false)
                 mainViewModel.isAgreeAll()
             }
-            textAgreeTwo.setOnClickListener {
+            layoutAgreeTwo.setOnClickListener {
                 mainViewModel.agreeTwo.value = !(mainViewModel.agreeTwo.value ?: false)
                 mainViewModel.isAgreeAll()
             }
-            textAgreeThree.setOnClickListener {
+            layoutAgreeThree.setOnClickListener {
                 mainViewModel.agreeThree.value = !(mainViewModel.agreeThree.value ?: false)
                 mainViewModel.isAgreeAll()
             }
-            textAgreeFour.setOnClickListener {
+            layoutAgreeFour.setOnClickListener {
                 mainViewModel.agreeFour.value = !(mainViewModel.agreeFour.value ?: false)
                 mainViewModel.isAgreeAll()
             }
-            textAgreeAll.setOnClickListener {
-                if (mainViewModel.agreeAll.value == true) {
-                    mainViewModel.setAgreeAll(false)
-                    mainViewModel.isAgreeAll()
-                } else {
-                    mainViewModel.setAgreeAll(true)
-                    mainViewModel.isAgreeAll()
-                }
-            }
-            checkboxAgreeOne.setOnClickListener {
-                mainViewModel.agreeOne.value = !(mainViewModel.agreeOne.value ?: false)
-                mainViewModel.isAgreeAll()
-            }
-            checkboxAgreeTwo.setOnClickListener {
-                mainViewModel.agreeTwo.value = !(mainViewModel.agreeTwo.value ?: false)
-                mainViewModel.isAgreeAll()
-            }
-            checkboxAgreeThree.setOnClickListener {
-                mainViewModel.agreeThree.value = !(mainViewModel.agreeThree.value ?: false)
-                mainViewModel.isAgreeAll()
-            }
-            checkboxAgreeFour.setOnClickListener {
-                mainViewModel.agreeFour.value = !(mainViewModel.agreeFour.value ?: false)
-                mainViewModel.isAgreeAll()
-            }
-            checkboxAgreeAll.setOnClickListener {
+            layoutAgreeAll.setOnClickListener {
                 if (mainViewModel.agreeAll.value == true) {
                     mainViewModel.setAgreeAll(false)
                     mainViewModel.isAgreeAll()
@@ -81,46 +55,63 @@ class AgreementPageActivity : BaseActivity<ActivityAgreementPageBinding>(
     }
 
     private fun observeData() {
-        mainViewModel.agreeAll.observe(this) {
-            binding.checkboxAgreeAll.isChecked = it
-        }
-        mainViewModel.agreeOne.observe(this) {
-            binding.checkboxAgreeOne.isChecked = it
-        }
-        mainViewModel.agreeTwo.observe(this) {
-            binding.checkboxAgreeTwo.isChecked = it
-        }
-        mainViewModel.agreeThree.observe(this) {
-            binding.checkboxAgreeThree.isChecked = it
-        }
-        mainViewModel.agreeFour.observe(this) {
-            binding.checkboxAgreeFour.isChecked = it
-        }
+        mainViewModel.agreeAll.observe(this, ::agreeAllWatcher)
+        mainViewModel.agreeOne.observe(this, ::agreeOneWatcher)
+        mainViewModel.agreeTwo.observe(this, ::agreeTwoWatcher)
+        mainViewModel.agreeThree.observe(this, ::agreeThreeWatcher)
+        mainViewModel.agreeFour.observe(this, ::agreeFourWatcher)
     }
 
-    private fun confirmBtnClickable(resource: Int, colorResource: Int, isClickable: Boolean) {
-        binding.btnConfirm.run {
-            this.setBackgroundResource(resource)
-            this.setTextColor(colorResource)
-            this.isClickable = isClickable
-        }
+    private fun agreeAllWatcher(isChecked: Boolean) {
+        binding.checkboxAgreeAll.isChecked = isChecked
+    }
+
+    private fun agreeOneWatcher(isChecked: Boolean) {
+        binding.checkboxAgreeOne.isChecked = isChecked
+    }
+
+    private fun agreeTwoWatcher(isChecked: Boolean) {
+        binding.checkboxAgreeTwo.isChecked = isChecked
+    }
+
+    private fun agreeThreeWatcher(isChecked: Boolean) {
+        binding.checkboxAgreeThree.isChecked = isChecked
+    }
+
+    private fun agreeFourWatcher(isChecked: Boolean) {
+        binding.checkboxAgreeFour.isChecked = isChecked
     }
 
     private fun isAgree() {
         mainViewModel.agreeAll.observe(this) { agree ->
             if (agree) {
                 confirmBtnClickable(
+                    binding.btnConfirm,
                     R.drawable.confirm_button_background,
-                    Color.WHITE,
+                    getColor(R.color.white),
                     true
                 )
             } else {
                 confirmBtnClickable(
+                    binding.btnConfirm,
                     R.drawable.disabled_btn_background,
-                    R.color.disabled_btn_text_color,
+                    getColor(R.color.disabled_btn_text_color),
                     false
                 )
             }
+        }
+    }
+
+    private fun confirmBtnClickable(
+        button: Button,
+        resource: Int,
+        colorResource: Int,
+        isClickable: Boolean
+    ) {
+        button.apply {
+            this.setBackgroundResource(resource)
+            this.setTextColor(colorResource)
+            this.isClickable = isClickable
         }
     }
 }
