7f72843e6b339bb520054b08f05892cfe4725ecc
parksb <oxen6297@naver.com>
2023-06-30 16:53:37 +0900

    feat: 더보기 fragment UI 구현

---
+++ SampleTwo/app/src/main/java/com/example/sampletwo/fragments/ShowMoreFragment.kt
@@ -0,0 +1,12 @@
+package com.example.sampletwo.fragments
+
+import android.os.Bundle
+import android.view.View
+import com.example.sampletwo.databinding.FragmentShowMoreBinding
+
+class ShowMoreFragment : BaseFragment<FragmentShowMoreBinding>(FragmentShowMoreBinding::inflate) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
