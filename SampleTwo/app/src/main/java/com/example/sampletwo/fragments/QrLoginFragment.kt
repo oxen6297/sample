c1e647f618a0c3a161800bf020abdbe397a5de6b
parksb <oxen6297@naver.com>
2023-07-03 10:13:31 +0900

    feat: qr 로그인 탭 구현

---
+++ SampleTwo/app/src/main/java/com/example/sampletwo/fragments/QrLoginFragment.kt
@@ -0,0 +1,14 @@
+package com.example.sampletwo.fragments
+
+import android.os.Bundle
+import android.view.View
+import com.example.sampletwo.databinding.FragmentQrLoginBinding
+
+
+class QrLoginFragment : BaseFragment<FragmentQrLoginBinding>(FragmentQrLoginBinding::inflate) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
