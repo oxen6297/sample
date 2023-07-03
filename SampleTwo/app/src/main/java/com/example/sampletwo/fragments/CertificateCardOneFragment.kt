72447c076b2c672d6038c6268cb04e5fa22b7376
parksb <oxen6297@naver.com>
2023-07-03 15:37:20 +0900

    feat: 메인화면 첫번째 카드 뷰페이저 ui 구현

---
+++ SampleTwo/app/src/main/java/com/example/sampletwo/fragments/CertificateCardOneFragment.kt
@@ -0,0 +1,19 @@
+package com.example.sampletwo.fragments
+
+import android.os.Bundle
+import android.view.View
+import androidx.navigation.fragment.findNavController
+import com.example.sampletwo.R
+import com.example.sampletwo.databinding.FragmentCertificateCardOneBinding
+
+class CertificateCardOneFragment :
+    BaseFragment<FragmentCertificateCardOneBinding>(FragmentCertificateCardOneBinding::inflate) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnGetIssue.setOnClickListener {
+            findNavController().navigate(R.id.certificateInfoFragment)
+        }
+    }
+}
