037a3729750958ff798b8e46ef84373df3fbfb42
parksb <oxen6297@naver.com>
2023-07-03 10:15:24 +0900

    feat: 바텀 네비게이션 구현

--- SampleTwo/app/src/main/java/com/example/sampletwo/activities/MainScreenActivity.kt
+++ SampleTwo/app/src/main/java/com/example/sampletwo/activities/MainScreenActivity.kt
@@ -5,6 +5,7 @@ import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.example.sampletwo.R
 import com.example.sampletwo.databinding.ActivityMainScreenBinding
+import com.example.sampletwo.util.CustomDialog
 
 class MainScreenActivity :
     BaseActivity<ActivityMainScreenBinding>(ActivityMainScreenBinding::inflate) {
@@ -15,5 +16,12 @@ class MainScreenActivity :
             supportFragmentManager.findFragmentById(R.id.fragment_container_view_main_screen) as NavHostFragment
         val navController = navHostFragment.navController
         binding.bottomNavMainScreen.setupWithNavController(navController)
+
+        navController.addOnDestinationChangedListener { controller, destination, _ ->
+            if (destination.id == R.id.qrLoginFragment) {
+                controller.navigate(R.id.certificateFragment)
+                CustomDialog(this, R.layout.custom_dialog).setDialog()
+            }
+        }
     }
 }
