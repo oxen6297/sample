ed25a5fa7a6ad3c4aab7de4091fc710d9d0d2de6
parksb <oxen6297@naver.com>
2023-07-03 10:15:58 +0900

    feat: 커스텀 다이얼로그 구현

---
+++ SampleTwo/app/src/main/java/com/example/sampletwo/util/CustomDialog.kt
@@ -0,0 +1,24 @@
+package com.example.sampletwo.util
+
+import android.annotation.SuppressLint
+import android.app.AlertDialog
+import android.content.Context
+import android.view.LayoutInflater
+import android.widget.Button
+import com.example.sampletwo.R
+
+@SuppressLint("InflateParams")
+class CustomDialog(context: Context,layoutResource: Int) {
+    private val view = LayoutInflater.from(context).inflate(layoutResource, null)
+    private val confirmBtn = view.findViewById<Button>(R.id.btn_confirm)
+    private val dialog = AlertDialog.Builder(context).setView(view)
+    private val builder = dialog.create()
+
+    @SuppressLint("ResourceAsColor")
+    fun setDialog() {
+        builder.show()
+        confirmBtn.setOnClickListener {
+            builder.dismiss()
+        }
+    }
+}
