5d0e3b2406519835d8b5a900833fbb1084fa2c4b
parksb <oxen6297@naver.com>
2023-07-03 15:37:55 +0900

    feat: 메인화면 두번째 카드 뷰페이저 ui 구현

---
+++ SampleTwo/app/src/main/java/com/example/sampletwo/fragments/CertificateCardTwoFragment.kt
@@ -0,0 +1,14 @@
+package com.example.sampletwo.fragments
+
+import android.os.Bundle
+import android.view.View
+import com.example.sampletwo.databinding.FragmentCertificateCardTwoBinding
+
+class CertificateCardTwoFragment :
+    BaseFragment<FragmentCertificateCardTwoBinding>(FragmentCertificateCardTwoBinding::inflate) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
