ad114ab8cf8df2c650c53dee5462aece532818cb
parksb <oxen6297@naver.com>
2023-07-03 10:17:14 +0900

    feat: dp px 변환 확장함수 구현

--- SampleTwo/app/src/main/java/com/example/sampletwo/extension/Extension.kt
+++ SampleTwo/app/src/main/java/com/example/sampletwo/extension/Extension.kt
@@ -1,5 +1,6 @@
 package com.example.sampletwo.extension
 
+import android.content.Context
 import android.widget.Button
 
 fun Button.clickable(
@@ -10,4 +11,9 @@ fun Button.clickable(
     this.setBackgroundResource(resource)
     this.setTextColor(colorResource)
     this.isClickable = isClickable
+}
+
+fun Context.convertDpToPixel(dp: Int): Int {
+    val density = resources.displayMetrics.density
+    return (density * dp).toInt()
 }
