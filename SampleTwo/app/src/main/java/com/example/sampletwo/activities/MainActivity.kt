82d7eee414611a6423a98c276ed3f2796d93aefd
parksb <oxen6297@naver.com>
2023-07-04 09:14:49 +0900

    fix: 스플래시 스크린 텍스트 일부 크기 및 색 변경

--- SampleTwo/app/src/main/java/com/example/sampletwo/activities/MainActivity.kt
+++ SampleTwo/app/src/main/java/com/example/sampletwo/activities/MainActivity.kt
@@ -1,14 +1,21 @@
 package com.example.sampletwo.activities
 
 import android.content.Intent
+import android.graphics.Typeface
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import com.example.sampletwo.R
 import com.example.sampletwo.databinding.ActivityMainBinding
+import com.example.sampletwo.extension.spannableStringBuilder
 
 class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::inflate) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding.textSplashLogoText.apply {
+            spannableStringBuilder(getColor(R.color.orange), 9, 15, Typeface.NORMAL, 1.25f)
+            spannableStringBuilder(getColor(R.color.orange), 19, 28, Typeface.NORMAL, 1.25f)
+        }
         Handler(Looper.getMainLooper()).postDelayed({
             Intent(this, MainScreenActivity::class.java).apply {
                 startActivity(this.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP))
