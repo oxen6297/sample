bdd02aaecbd5726eb00aca871d6623678c81f175
parksb <oxen6297@naver.com>
2023-07-04 09:05:55 +0900

    feat: 공무원 연금증 발급 안내 첫번째 페이지 ui 구현

---
+++ SampleTwo/app/src/main/java/com/example/sampletwo/fragments/CertificateInfoFragment.kt
@@ -0,0 +1,24 @@
+package com.example.sampletwo.fragments
+
+import android.os.Bundle
+import android.view.View
+import androidx.navigation.fragment.findNavController
+import com.example.sampletwo.R
+import com.example.sampletwo.databinding.FragmentCertificateInfoBinding
+
+class CertificateInfoFragment :
+    BaseFragment<FragmentCertificateInfoBinding>(FragmentCertificateInfoBinding::inflate) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.apply {
+            appBar.imgBack.setOnClickListener {
+                findNavController().popBackStack()
+            }
+            btnNext.setOnClickListener {
+                findNavController().navigate(R.id.certificationInfoTwoFragment)
+            }
+        }
+    }
+}
