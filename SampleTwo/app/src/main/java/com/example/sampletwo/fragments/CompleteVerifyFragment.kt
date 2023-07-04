18483c53a916d959e32c2b5bc687f904cfc72a35
parksb <oxen6297@naver.com>
2023-07-04 09:10:42 +0900

    feat: 본인인증 완료 페이지 ui 구현

---
+++ SampleTwo/app/src/main/java/com/example/sampletwo/fragments/CompleteVerifyFragment.kt
@@ -0,0 +1,19 @@
+package com.example.sampletwo.fragments
+
+import android.os.Bundle
+import android.view.View
+import androidx.navigation.fragment.findNavController
+import com.example.sampletwo.R
+import com.example.sampletwo.databinding.FragmentCompleteVerifyBinding
+
+class CompleteVerifyFragment :
+    BaseFragment<FragmentCompleteVerifyBinding>(FragmentCompleteVerifyBinding::inflate) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnConfirm.setOnClickListener {
+            findNavController().navigate(R.id.certificateInfoThreeFragment)
+        }
+    }
+}
