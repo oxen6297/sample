c0aa1d59318c3e071b4c6abe97aa12604e600857
parksb <oxen6297@naver.com>
2023-07-05 14:45:52 +0900

    refactor: custom dialog background 투명 설정

--- SampleTwo/app/src/main/java/com/example/sampletwo/util/CustomDialog.kt
+++ SampleTwo/app/src/main/java/com/example/sampletwo/util/CustomDialog.kt
@@ -3,6 +3,8 @@ package com.example.sampletwo.util
 import android.annotation.SuppressLint
 import android.app.AlertDialog
 import android.content.Context
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.view.LayoutInflater
 import android.widget.Button
 import com.example.sampletwo.R
@@ -12,6 +14,10 @@ class CustomDialog(context: Context, layoutResource: Int) {
     private val view = LayoutInflater.from(context).inflate(layoutResource, null)
     private val confirmBtn = view.findViewById<Button>(R.id.btn_confirm)
     private val dialog = AlertDialog.Builder(context).setView(view).create()
+
+    init {
+        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+    }
 
     fun setDialog() {
         dialog.show()
