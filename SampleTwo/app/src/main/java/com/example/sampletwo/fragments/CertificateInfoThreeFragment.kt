e159cf8f04c7ae6d12621ecb7c217da461749ca0
parksb <oxen6297@naver.com>
2023-07-07 14:24:36 +0900

    feat: 권한 및 카메라 켜기 기능 구현

--- SampleTwo/app/src/main/java/com/example/sampletwo/fragments/CertificateInfoThreeFragment.kt
+++ SampleTwo/app/src/main/java/com/example/sampletwo/fragments/CertificateInfoThreeFragment.kt
@@ -1,24 +1,43 @@
 package com.example.sampletwo.fragments
 
+import android.Manifest
 import android.annotation.SuppressLint
+import android.app.Activity.RESULT_OK
+import android.content.Context
+import android.content.Intent
 import android.graphics.Typeface
 import android.os.Bundle
+import android.provider.MediaStore
 import android.text.SpannableStringBuilder
 import android.text.style.ForegroundColorSpan
 import android.text.style.StyleSpan
 import android.view.View
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.sampletwo.R
 import com.example.sampletwo.databinding.FragmentCertificateInfoThreeBinding
+import com.example.sampletwo.extension.showToast
 import com.example.sampletwo.util.IndentLeadingMarginSpan
+import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.normal.TedPermission
 
 class CertificateInfoThreeFragment :
     BaseFragment<FragmentCertificateInfoThreeBinding>(FragmentCertificateInfoThreeBinding::inflate) {
+
+    private lateinit var cameraLauncher: ActivityResultLauncher<Intent>
+    private lateinit var requestPermissionLauncher: ActivityResultLauncher<String>
+
     @SuppressLint("ResourceAsColor")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initRequestPermissionLauncher(view.context)
+        initCameraLauncher()
+        setUpBinding(view.context)
+    }
 
-        val blueColor = view.context.getColor(R.color.confirm_btn_color)
-        val redColor = view.context.getColor(R.color.red)
+    private fun setUpBinding(context: Context) {
+        val blueColor = context.getColor(R.color.confirm_btn_color)
+        val redColor = context.getColor(R.color.red)
 
         binding.apply {
             textInfoCameraIdCardContent.run {
@@ -57,6 +76,59 @@ class CertificateInfoThreeFragment :
                 }
                 text = builder
             }
+            btnCamera.setOnClickListener {
+//                requestTedPermission()
+                requestPermission()
+            }
         }
+    }
+
+    private fun initCameraLauncher() {
+        cameraLauncher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+                if (it.resultCode == RESULT_OK) {
+                    //사진 처리
+                }
+            }
+    }
+
+    private fun cameraLauncher() {
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        cameraLauncher.launch(intent)
+    }
+
+    /**
+        일반 권한 요청입니다.
+     **/
+    private fun requestPermission() {
+        val permissionList = Manifest.permission.CAMERA
+        requestPermissionLauncher.launch(permissionList)
+    }
+
+    private fun initRequestPermissionLauncher(context: Context) {
+        requestPermissionLauncher =
+            registerForActivityResult(ActivityResultContracts.RequestPermission()) {
+                if (it) cameraLauncher()
+                else context.showToast("권한을 허용해주세요.")
+            }
+    }
+
+    /**
+        테드퍼미션 사용한 권한 요청입니다.
+     **/
+    private fun requestTedPermission() {
+        TedPermission.create()
+            .setPermissionListener(object : PermissionListener {
+                override fun onPermissionGranted() {
+                    cameraLauncher()
+                }
+
+                override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
+
+                }
+            })
+            .setDeniedMessage("권한을 허용해주세요.")
+            .setPermissions(Manifest.permission.CAMERA)
+            .check()
     }
 }
