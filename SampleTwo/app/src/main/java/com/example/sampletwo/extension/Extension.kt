88918994f8ec5c61973c85cac15c729acb222a3a
parksb <oxen6297@naver.com>
2023-07-07 11:30:58 +0900

    refactor: spannableStringBuilder, leading margin 사용 변경 및 확장 함수 제거

--- SampleTwo/app/src/main/java/com/example/sampletwo/extension/Extension.kt
+++ SampleTwo/app/src/main/java/com/example/sampletwo/extension/Extension.kt
@@ -1,13 +1,7 @@
 package com.example.sampletwo.extension
 
 import android.content.Context
-import android.graphics.Typeface
-import android.text.SpannableStringBuilder
-import android.text.Spanned
 import android.view.View
-import android.widget.TextView
-import com.example.sampletwo.util.IndentLeadingMarginSpan
-import com.example.sampletwo.util.RelativeSizeTypeSpan
 
 fun View.visibilityGone() {
     visibility = View.GONE
@@ -20,32 +14,4 @@ fun View.visibilityVisible() {
 fun Context.convertDpToPixel(dp: Int): Int {
     val density = resources.displayMetrics.density
     return (density * dp).toInt()
-}
-
-fun TextView.spannableStringBuilder(
-    colorResource: Int,
-    startIndex: Int,
-    endIndex: Int,
-    typeResource: Int = Typeface.BOLD,
-    textSize: Float = 1f
-) {
-    val builder = SpannableStringBuilder(text)
-    builder.setSpan(
-        RelativeSizeTypeSpan(textSize, colorResource, typeResource),
-        startIndex,
-        endIndex,
-        Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
-    )
-    text = builder
-}
-
-fun TextView.spannableIndentMarginBuilder(marginText: String) {
-    val builder = SpannableStringBuilder(text)
-    builder.setSpan(
-        IndentLeadingMarginSpan(marginText),
-        0,
-        text.length,
-        Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
-    )
-    text = builder
 }
--- SampleTwo/app/src/main/java/com/example/sampletwo/fragments/CertificateInfoThreeFragment.kt
+++ SampleTwo/app/src/main/java/com/example/sampletwo/fragments/CertificateInfoThreeFragment.kt
@@ -3,15 +3,16 @@ package com.example.sampletwo.fragments
 import android.annotation.SuppressLint
 import android.graphics.Typeface
 import android.os.Bundle
+import android.text.SpannableStringBuilder
+import android.text.style.ForegroundColorSpan
+import android.text.style.StyleSpan
 import android.view.View
 import com.example.sampletwo.R
 import com.example.sampletwo.databinding.FragmentCertificateInfoThreeBinding
-import com.example.sampletwo.extension.spannableIndentMarginBuilder
-import com.example.sampletwo.extension.spannableStringBuilder
+import com.example.sampletwo.util.IndentLeadingMarginSpan
 
 class CertificateInfoThreeFragment :
     BaseFragment<FragmentCertificateInfoThreeBinding>(FragmentCertificateInfoThreeBinding::inflate) {
-
     @SuppressLint("ResourceAsColor")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -20,21 +21,42 @@ class CertificateInfoThreeFragment :
         val redColor = view.context.getColor(R.color.red)
 
         binding.apply {
-            textInfoCameraIdCardContent.apply {
-                spannableStringBuilder(blueColor, 29, 35)
-                spannableStringBuilder(blueColor, 38, 44)
+            textInfoCameraIdCardContent.run {
+                val builder = SpannableStringBuilder(text).apply {
+                    setSpan(ForegroundColorSpan(blueColor), 29, 35, 0)
+                    setSpan(ForegroundColorSpan(blueColor), 38, 44, 0)
+                    setSpan(StyleSpan(Typeface.BOLD), 29, 35, 0)
+                    setSpan(StyleSpan(Typeface.BOLD), 38, 44, 0)
+                }
+                text = builder
             }
-            textCautionContent.apply {
-                spannableStringBuilder(blueColor, 1, 7)
-                spannableStringBuilder(blueColor, 30, 37)
-                spannableStringBuilder(blueColor, 39, 46)
-                spannableStringBuilder(blueColor, 58, 64)
-                spannableStringBuilder(blueColor, 80, 86)
-                spannableStringBuilder(blueColor, 115, 122)
-                spannableStringBuilder(blueColor, 155, 162)
-                spannableIndentMarginBuilder("· ")
+            textCaution.run {
+                val builder = SpannableStringBuilder(text).apply {
+                    setSpan(ForegroundColorSpan(redColor), 0, 2, 0)
+                    setSpan(StyleSpan(Typeface.BOLD), 0, 2, 0)
+                }
+                text = builder
             }
-            textCaution.spannableStringBuilder(redColor, 0, 2, Typeface.NORMAL)
+            textCautionContent.run {
+                val builder = SpannableStringBuilder(text).apply {
+                    setSpan(ForegroundColorSpan(blueColor), 1, 7, 0)
+                    setSpan(ForegroundColorSpan(blueColor), 30, 37, 0)
+                    setSpan(ForegroundColorSpan(blueColor), 39, 46, 0)
+                    setSpan(ForegroundColorSpan(blueColor), 58, 64, 0)
+                    setSpan(ForegroundColorSpan(blueColor), 80, 86, 0)
+                    setSpan(ForegroundColorSpan(blueColor), 115, 122, 0)
+                    setSpan(ForegroundColorSpan(blueColor), 155, 162, 0)
+                    setSpan(StyleSpan(Typeface.BOLD), 1, 7, 0)
+                    setSpan(StyleSpan(Typeface.BOLD), 30, 37, 0)
+                    setSpan(StyleSpan(Typeface.BOLD), 39, 46, 0)
+                    setSpan(StyleSpan(Typeface.BOLD), 58, 64, 0)
+                    setSpan(StyleSpan(Typeface.BOLD), 80, 86, 0)
+                    setSpan(StyleSpan(Typeface.BOLD), 115, 122, 0)
+                    setSpan(StyleSpan(Typeface.BOLD), 155, 162, 0)
+                    setSpan(IndentLeadingMarginSpan("· "), 0, text.length, 0)
+                }
+                text = builder
+            }
         }
     }
 }
