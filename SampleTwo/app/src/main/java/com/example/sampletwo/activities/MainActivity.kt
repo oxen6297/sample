58343938f47c3a46fccca53da4db602d27836a32
parksb <oxen6297@naver.com>
2023-07-07 11:31:51 +0900

    refactor: 스플래시 스크린 spannableStringBuilder 사용 변경

--- SampleTwo/app/src/main/java/com/example/sampletwo/activities/MainActivity.kt
+++ SampleTwo/app/src/main/java/com/example/sampletwo/activities/MainActivity.kt
@@ -1,20 +1,26 @@
 package com.example.sampletwo.activities
 
 import android.content.Intent
-import android.graphics.Typeface
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.text.SpannableStringBuilder
+import android.text.style.ForegroundColorSpan
+import android.text.style.RelativeSizeSpan
 import com.example.sampletwo.R
 import com.example.sampletwo.databinding.ActivityMainBinding
-import com.example.sampletwo.extension.spannableStringBuilder
 
 class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::inflate) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding.textSplashLogoText.apply {
-            spannableStringBuilder(getColor(R.color.orange), 9, 15, Typeface.NORMAL, 1.25f)
-            spannableStringBuilder(getColor(R.color.orange), 19, 28, Typeface.NORMAL, 1.25f)
+        binding.textSplashLogoText.run {
+            val builder = SpannableStringBuilder(text).apply {
+                setSpan(ForegroundColorSpan(getColor(R.color.orange)), 9, 15, 0)
+                setSpan(ForegroundColorSpan(getColor(R.color.orange)), 19, 28, 0)
+                setSpan(RelativeSizeSpan(1.25f), 9, 15, 0)
+                setSpan(RelativeSizeSpan(1.25f), 19, 28, 0)
+            }
+            text = builder
         }
         Handler(Looper.getMainLooper()).postDelayed({
             Intent(this, MainScreenActivity::class.java).apply {
