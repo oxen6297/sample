52a0b3222c567abff61206f6cd2dd93388456bea
parksb <oxen6297@naver.com>
2023-07-10 10:41:20 +0900

    feat: hilt 라이브러리 추가 및 적용

---
+++ SampleTwo/app/src/main/java/com/example/sampletwo/HiltApplication.kt
@@ -0,0 +1,18 @@
+package com.example.sampletwo
+
+import android.app.Application
+import android.content.Context
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class HiltApplication : Application() {
+
+    companion object {
+        var appContext: Context? = null
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        appContext = this
+    }
+}
