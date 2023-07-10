f8a83850349c54b398e146fbb412e7d45f2b6820
parksb <oxen6297@naver.com>
2023-07-10 18:10:43 +0900

    rename: viewpager 패키지로 이동

--- SampleTwo/app/src/main/java/com/example/sampletwo/adapter/ViewPagerAdapter.kt
+++ SampleTwo/app/src/main/java/com/example/sampletwo/adapter/ViewPagerAdapter.kt
@@ -3,8 +3,8 @@ package com.example.sampletwo.adapter
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
-import com.example.sampletwo.fragments.CertificateCardOneFragment
-import com.example.sampletwo.fragments.CertificateCardTwoFragment
+import com.example.sampletwo.fragments.viewpager.CertificateCardOneFragment
+import com.example.sampletwo.fragments.viewpager.CertificateCardTwoFragment
 
 class ViewPagerAdapter(activity: FragmentActivity) : FragmentStateAdapter(activity) {
     override fun getItemCount(): Int = 2
