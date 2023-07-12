09dfb6ad09185e7b0b321eb71537aa5be32d5ee6
parksb <oxen6297@naver.com>
2023-07-12 17:06:19 +0900

    refactor: 함수 이름 변경 및 커스텀 다이얼로그 확장함수 구현

--- SampleTwo/app/src/main/java/com/example/sampletwo/extension/Extension.kt
+++ SampleTwo/app/src/main/java/com/example/sampletwo/extension/Extension.kt
@@ -2,21 +2,51 @@ package com.example.sampletwo.extension
 
 import android.content.Context
 import android.view.View
-import android.widget.Toast
+import com.example.sampletwo.R
+import com.example.sampletwo.util.CustomDialog
+import com.example.sampletwo.util.CustomDialogTwoButton
 
-fun View.visibilityGone() {
+fun View.hide() {
     visibility = View.GONE
 }
 
-fun View.visibilityVisible() {
+fun View.show() {
     visibility = View.VISIBLE
 }
 
-fun Context.convertDpToPixel(dp: Int): Int {
+fun Context.dpToPx(dp: Int): Int {
     val density = resources.displayMetrics.density
     return (density * dp).toInt()
 }
 
-fun Context.showToast(message: String, duration: Int = 0) {
-    Toast.makeText(this, message, duration).show()
+fun Context.customDialog(textViewText: Int, btnText: Int): CustomDialog {
+    return CustomDialog(this, R.layout.custom_dialog).apply {
+        initView()
+        setViewComponent(R.id.btn_confirm, R.id.text_dialog_certificate)
+        showDialog()
+        textContent.text = getString(textViewText)
+        confirmBtn.text = getString(btnText)
+        confirmBtn()
+    }
+}
+
+fun Context.customDialogTwoButton(
+    confirmBtnText: Int,
+    cancelBtnText: Int,
+    titleText: Int,
+    contentText: Int,
+    clickConfirm: (View) -> Unit,
+    clickCancel: (View) -> Unit = {},
+): CustomDialogTwoButton {
+    return CustomDialogTwoButton(this, R.layout.custom_dialog_two_button).apply {
+        initView()
+        setViewComponent(R.id.btn_confirm, R.id.btn_cancel, R.id.text_title, R.id.text_content)
+        showDialog()
+        confirmBtn.text = getString(confirmBtnText)
+        cancelBtn.text = getString(cancelBtnText)
+        textTitle.text = getString(titleText)
+        textContent.text = getString(contentText)
+        cancelBtn(clickCancel)
+        confirmBtn(clickConfirm)
+    }
 }
