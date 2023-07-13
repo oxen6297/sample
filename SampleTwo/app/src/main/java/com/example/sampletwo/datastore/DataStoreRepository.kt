af722cc6b02fc0402cb94bfbfa8c1b9f9bdcea77
parksb <oxen6297@naver.com>
2023-07-13 14:44:17 +0900

    refactor: dataStore key 수정

--- SampleTwo/app/src/main/java/com/example/sampletwo/datastore/DataStoreRepository.kt
+++ SampleTwo/app/src/main/java/com/example/sampletwo/datastore/DataStoreRepository.kt
@@ -13,33 +13,33 @@ class DataStoreRepository(private val context: Context) : UserInfoService {
 
     override suspend fun saveUserInfo(userInfo: UserInfo) {
         context.dataStore.edit { userInfoSet ->
-            userInfoSet[IMAGE] = userInfo.image
-            userInfoSet[NAME] = userInfo.name
-            userInfoSet[CERTIFICATE_NUMBER] = userInfo.certificateNumber
-            userInfoSet[BIRTH] = userInfo.birth
-            userInfoSet[CERTIFICATE_DATE] = userInfo.certificateDate
-            userInfoSet[RECENT_COMPANY] = userInfo.recentCompany
+            userInfoSet[image] = userInfo.image
+            userInfoSet[name] = userInfo.name
+            userInfoSet[certificateNumber] = userInfo.certificateNumber
+            userInfoSet[birth] = userInfo.birth
+            userInfoSet[certificateDate] = userInfo.certificateDate
+            userInfoSet[recentCompany] = userInfo.recentCompany
         }
     }
 
     override suspend fun readUserInfo(): Flow<UserInfo> = context.dataStore.data.map { userInfo ->
         UserInfo(
-            image = userInfo[IMAGE] ?: "",
-            name = userInfo[NAME] ?: "",
-            certificateNumber = userInfo[CERTIFICATE_NUMBER] ?: "",
-            birth = userInfo[BIRTH] ?: "",
-            certificateDate = userInfo[CERTIFICATE_DATE] ?: 0L,
-            recentCompany = userInfo[RECENT_COMPANY] ?: ""
+            image = userInfo[image] ?: "",
+            name = userInfo[name] ?: "",
+            certificateNumber = userInfo[certificateNumber] ?: "",
+            birth = userInfo[birth] ?: "",
+            certificateDate = userInfo[certificateDate] ?: 0L,
+            recentCompany = userInfo[recentCompany] ?: ""
         )
     }
 
     companion object {
         private const val DATA_STORE_NAME = "user_info"
-        private val IMAGE = stringPreferencesKey("image")
-        private val NAME = stringPreferencesKey("name")
-        private val CERTIFICATE_NUMBER = stringPreferencesKey("certificateNumber")
-        private val BIRTH = stringPreferencesKey("birth")
-        private val CERTIFICATE_DATE = longPreferencesKey("certificateDate")
-        private val RECENT_COMPANY = stringPreferencesKey("recentCompany")
+        private val image = stringPreferencesKey(IMAGE)
+        private val name = stringPreferencesKey(NAME)
+        private val certificateNumber = stringPreferencesKey(CERTIFICATE_NUMBER)
+        private val birth = stringPreferencesKey(BIRTH)
+        private val certificateDate = longPreferencesKey(CERTIFICATE_DATE)
+        private val recentCompany = stringPreferencesKey(RECENT_COMPANY)
     }
 }
