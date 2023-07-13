3491da4f2266b1ddc8f2625610c6ec620ba6d4dc
parksb <oxen6297@naver.com>
2023-07-13 14:43:26 +0900

    feat: dataStore key 생성

---
+++ SampleTwo/app/src/main/java/com/example/sampletwo/datastore/preferenceKey.kt
@@ -0,0 +1,8 @@
+package com.example.sampletwo.datastore
+
+const val IMAGE = "image"
+const val NAME = "name"
+const val CERTIFICATE_NUMBER = "certificateNumber"
+const val BIRTH = "birth"
+const val CERTIFICATE_DATE = "certificateDate"
+const val RECENT_COMPANY = "recentCompany"
