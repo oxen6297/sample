4551a363bddb136bbd1f2be6a474285e5835a648
parksb <oxen6297@naver.com>
2023-07-21 16:57:05 +0900

    feat: 권한 팝업 확장함수 구현

--- SampleTwo/app/src/main/java/com/example/sampletwo/extension/PopUpExtension.kt
+++ SampleTwo/app/src/main/java/com/example/sampletwo/extension/PopUpExtension.kt
@@ -1,6 +1,9 @@
 package com.example.sampletwo.extension
 
 import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.provider.Settings
 import android.view.View
 import com.example.sampletwo.R
 
@@ -10,4 +13,18 @@ fun Context.qrPopUp(clickConfirm: (View) -> Unit = {}) {
         btnText = R.string.confirm_btn_text,
         clickConfirm = clickConfirm
     )
+}
+
+fun Context.permissionPopUp() {
+    customDialogTwoButton(
+        R.string.go_setting,
+        R.string.dialog_cancel,
+        R.string.permission_title,
+        R.string.permission_content,
+        {
+            Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS).apply {
+                data = Uri.parse("package:" + this@permissionPopUp.packageName)
+                startActivity(this)
+            }
+        })
 }
