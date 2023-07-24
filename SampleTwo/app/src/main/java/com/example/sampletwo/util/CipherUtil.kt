6da95d1caeb922fe10a94108d2c8f59f1a7968aa
parksb <oxen6297@naver.com>
2023-07-24 10:33:02 +0900

    feat: Cipher 암호화 util class 생성

---
+++ SampleTwo/app/src/main/java/com/example/sampletwo/util/CipherUtil.kt
@@ -0,0 +1,36 @@
+package com.example.sampletwo.util
+
+import android.os.Build
+import androidx.annotation.RequiresApi
+import java.nio.charset.StandardCharsets
+import java.util.Base64
+import javax.crypto.Cipher
+import javax.crypto.spec.IvParameterSpec
+import javax.crypto.spec.SecretKeySpec
+
+@RequiresApi(Build.VERSION_CODES.O)
+class CipherUtil {
+
+    fun encrypt(plainText: String): String {
+        val key = SecretKeySpec(SECRET_KEY.toByteArray(),"AES")
+        val iv = IvParameterSpec(IV.toByteArray())
+        val cipher: Cipher = Cipher.getInstance("AES/CBC/PKCS5Padding")
+        cipher.init(Cipher.ENCRYPT_MODE, key, iv)
+        val encrypted: ByteArray = cipher.doFinal(plainText.toByteArray(StandardCharsets.UTF_8))
+        return String(Base64.getEncoder().encode(encrypted))
+    }
+
+    fun decrypt(cipherText: String?): String {
+        val key = SecretKeySpec(SECRET_KEY.toByteArray(),"AES")
+        val iv = IvParameterSpec(IV.toByteArray())
+        val cipher: Cipher = Cipher.getInstance("AES/CBC/PKCS5Padding")
+        cipher.init(Cipher.DECRYPT_MODE, key, iv)
+        val decrypted: ByteArray = cipher.doFinal(Base64.getDecoder().decode(cipherText))
+        return String(decrypted, StandardCharsets.UTF_8)
+    }
+
+    companion object{
+        private const val SECRET_KEY = "01234567890123450123456789012345"
+        private const val IV = "0123456789012345"
+    }
+}
