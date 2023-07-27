13450253d7b55752b51b426d13853f1f6da44b16
parksb <oxen6297@naver.com>
2023-07-28 08:57:44 +0900

    refactor: 웹뷰 뒤로가기 다이얼로그 구현

--- SampleTwo/app/src/main/java/com/example/sampletwo/fragments/VerifyFragment.kt
+++ SampleTwo/app/src/main/java/com/example/sampletwo/fragments/VerifyFragment.kt
@@ -4,8 +4,10 @@ import android.annotation.SuppressLint
 import android.content.ActivityNotFoundException
 import android.content.Context
 import android.content.Intent
+import android.graphics.Bitmap
 import android.net.Uri
 import android.view.View
+import android.view.WindowManager
 import android.webkit.WebChromeClient
 import android.webkit.WebResourceRequest
 import android.webkit.WebSettings
@@ -17,6 +19,9 @@ import androidx.fragment.app.viewModels
 import androidx.navigation.findNavController
 import com.example.sampletwo.R
 import com.example.sampletwo.databinding.FragmentVerifyBinding
+import com.example.sampletwo.extension.customDialogTwoButton
+import com.example.sampletwo.extension.hide
+import com.example.sampletwo.extension.show
 import com.example.sampletwo.viewmodels.MainViewModel
 
 class VerifyFragment :
@@ -30,7 +35,15 @@ class VerifyFragment :
             override fun handleOnBackPressed() {
                 binding.webView.apply {
                     if (canGoBack()) goBack()
-                    else findNavController().popBackStack()
+                    else {
+                        context.customDialogTwoButton(
+                            R.string.confirm_btn_text,
+                            R.string.cancel,
+                            R.string.verify,
+                            R.string.verify_dialog_content,
+                            { findNavController().popBackStack() }
+                        )
+                    }
                 }
             }
         }
@@ -58,8 +71,8 @@ class VerifyFragment :
                     request: WebResourceRequest?
                 ): Boolean {
                     val url = request?.url.toString()
+                    val intent = Intent.parseUri(url, Intent.URI_INTENT_SCHEME)
                     return if (url.startsWith("intent://")) {
-                        val intent = Intent.parseUri(url, Intent.URI_INTENT_SCHEME)
                         try {
                             startActivity(intent)
                         } catch (e: ActivityNotFoundException) {
@@ -74,6 +87,21 @@ class VerifyFragment :
                         }
                         true
                     } else false
+                }
+
+                override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+                    super.onPageStarted(view, url, favicon)
+                    requireActivity().window.setFlags(
+                        WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE,
+                        WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE
+                    )
+                    binding.progressbar.show()
+                }
+
+                override fun onPageFinished(view: WebView?, url: String?) {
+                    super.onPageFinished(view, url)
+                    binding.progressbar.hide()
+                    requireActivity().window.clearFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE)
                 }
             }
             loadUrl("https://dev.adtcapsmobilecard.co.kr/nice/checkplus_main?os_type=1&app_type=A")
