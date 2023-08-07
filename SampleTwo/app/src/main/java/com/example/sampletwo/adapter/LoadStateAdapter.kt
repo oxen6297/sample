6770731aebbc082d6a5d6dbdf97dca8a9d3b571d
oxen6297 <112065694+oxen6297@users.noreply.github.com>
2023-08-07 16:23:35 +0900

    feat: loadStateAdapter 구현

---
+++ SampleTwo/app/src/main/java/com/example/sampletwo/adapter/LoadStateAdapter.kt
@@ -0,0 +1,33 @@
+package com.example.sampletwo.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.core.view.isVisible
+import androidx.paging.LoadState
+import androidx.recyclerview.widget.RecyclerView
+import com.example.sampletwo.databinding.LoadStateItemBinding
+import com.example.sampletwo.extension.showToast
+
+class LoadStateAdapter : androidx.paging.LoadStateAdapter<LoadStateAdapter.ViewHolder>() {
+
+    class ViewHolder(val binding: LoadStateItemBinding) :
+        RecyclerView.ViewHolder(binding.root)
+
+    override fun onBindViewHolder(holder: ViewHolder, loadState: LoadState) {
+        holder.binding.apply {
+            progressbar.isVisible = loadState is LoadState.Loading
+            if (loadState is LoadState.Error) {
+                root.context.showToast("에러가 발생하였습니다.")
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, loadState: LoadState): ViewHolder {
+        val binding = LoadStateItemBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
+        return ViewHolder(binding)
+    }
+}
